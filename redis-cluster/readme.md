a3790331ddaae94ce777c259f18c45b8d22e8b67
Qicz <qiczzhu@gmail.com>
2021-09-29 18:42:57 +0800

    update redis cluster config.

--- redis-cluster/readme.md
+++ redis-cluster/readme.md
@@ -1,7 +1,1 @@
-### How?
-
-- put all file to redis root dir.
-- start cluster: `sh start-redis-cluster.sh`
-- create cluster:`sh create-cluster.sh`
-- open cluster client: `sh start-redis-cli.sh`
-
+put all file to redis root dir.
--- redis-cluster/start-redis-cluster.sh
+++ redis-cluster/start-redis-cluster.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir node-7291 node-7292 node-7293 node-7294 node-7295 node-7296
 ./src/redis-server redis-cluster/7291/redis.conf 
 ./src/redis-server redis-cluster/7292/redis.conf
 ./src/redis-server redis-cluster/7293/redis.conf
